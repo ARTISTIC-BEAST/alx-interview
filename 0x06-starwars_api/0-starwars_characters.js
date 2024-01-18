a44a61f07439a595d2ad947de150be8159881069
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2024-01-18 23:19:34 +0000

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,42 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
-
 const movie = process.argv[2];
-
-
-function doRequest() {
-  return new Promise(function (resolve, reject) {
-    request(`https://swapi-api.hbtn.io/api/films/${movie}`, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-
-        resolve(JSON.parse(body).characters);
-      } else {
-        reject(error);
-      }
-
-    });
-  })
-}
-
-function doRequest2(characters) {
-  return new Promise(function (resolve, reject) {
-    request(characters, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        resolve(JSON.parse(body));
-      } else {
-        reject(error);
-      }
-
-    });
-  })
-}
-
-
-(async () => {
-  const re = await doRequest();
-
-  for (const element of re) {
-    let res = await doRequest2(element);
-    console.log(res.name);
+request(`https://swapi-api.hbtn.io/api/films/${movie}`, function (error, response, body) {
+  if (error) {
+    return console.log(error);
   }
-})();
+  const characters = JSON.parse(body).characters;
+  characters.forEach(element => {
+    request(element, function (error, response, body) {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(JSON.parse(body).name);
+    });
+  });
+});
