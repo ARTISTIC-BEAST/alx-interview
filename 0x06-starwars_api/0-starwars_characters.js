7b2f9f21126e757061521af28201b28470e68f74
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2024-01-18 23:18:17 +0000

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,58 +1,42 @@
 #!/usr/bin/node
 const request = require('request');
 
-function getMovieCharacters(movieId) {
-  const url = `https://swapi.dev/api/films/${movieId}/`;
+const movie = process.argv[2];
 
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        const movieData = JSON.parse(body);
-        const charactersURLs = movieData.characters;
 
-        // Fetch character names from the characters' URLs
-        const charactersPromises = charactersURLs.map(characterURL =>
-          new Promise((resolveChar, rejectChar) => {
-            request(characterURL, (errorChar, responseChar, bodyChar) => {
-              if (!errorChar && responseChar.statusCode === 200) {
-                const characterData = JSON.parse(bodyChar);
-                resolveChar(characterData.name);
-              } else {
-                rejectChar(`Error fetching character details: ${characterURL}`);
-              }
-            });
-          })
-        );
+function doRequest() {
+  return new Promise(function (resolve, reject) {
+    request(`https://swapi-api.hbtn.io/api/films/${movie}`, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
 
-        // Resolve all character promises
-        Promise.all(charactersPromises)
-          .then(characters => resolve(characters))
-          .catch(error => reject(error));
+        resolve(JSON.parse(body).characters);
       } else {
-        reject(`Error fetching movie details: ${url}`);
+        reject(error);
       }
+
     });
-  });
+  })
 }
 
-function main() {
-  // Check if the Movie ID is provided as a command-line argument
-  if (process.argv.length !== 3) {
-    console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-    process.exit(1);
+function doRequest2(characters) {
+  return new Promise(function (resolve, reject) {
+    request(characters, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        resolve(JSON.parse(body));
+      } else {
+        reject(error);
+      }
+
+    });
+  })
+}
+
+
+(async () => {
+  const re = await doRequest();
+
+  for (const element of re) {
+    let res = await doRequest2(element);
+    console.log(res.name);
   }
-
-  const movieId = process.argv[2];
-
-  // Get and print the characters for the specified movie
-  getMovieCharacters(movieId)
-    .then(characters => {
-      characters.forEach(character => console.log(character));
-    })
-    .catch(error => console.error(error));
-}
-
-if (require.main === module) {
-  main();
-}
-
+})();
